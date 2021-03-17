34ac8d39b9fc57345b44b35ef85af1a88ae531ab
Ahmet Zurnaci <cengamat@gmail.com>
2021-03-17 12:25:02 +0000

    Passing method references between components

--- react-zone-2/src/App.js
+++ react-zone-2/src/App.js
@@ -13,12 +13,12 @@ class App extends Component{
     otherState : 'Ahmet'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Expecto Patronum!');
 
     this.setState({
       persons : [
-        { name : "Ahmet", age: 25},
+        { name : newName, age: 25},
         { name : "Levent", age: 25},
         { name : "Ayse", age: 55},
         { name : "", age: 0}
@@ -30,8 +30,8 @@ class App extends Component{
     return(
       <div className="App">
         <h1>Hello!</h1>
-        <button onClick = {this.switchNameHandler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}>My Hobbie : Surfing</Person>
+        <button onClick = {this.switchNameHandler.bind(this, 'Ahmet')}>Switch Name</button>
+        <Person click = {this.switchNameHandler.bind(this, 'Amat')} name = {this.state.persons[0].name} age = {this.state.persons[0].age}>My Hobbie : Surfing</Person>
         <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
         <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
         <Person name = {this.state.persons[3].name} age = {this.state.persons[3].age} />
