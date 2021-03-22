b82944b2b56d32f9e39a53dca7354ab077f4ea72
Ahmet Zurnaci <cengamat@gmail.com>
2021-03-22 10:29:55 +0000

    Setting styles dynamically

--- react-zone-2/src/App.js
+++ react-zone-2/src/App.js
@@ -47,7 +47,7 @@ class App extends Component{
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -64,6 +64,7 @@ class App extends Component{
             })}
         </div>
       );
+      style.backgroundColor = 'red';
     }
 
     return(
