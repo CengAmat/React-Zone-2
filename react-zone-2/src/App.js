b0f79048b500798dfed72dce36af66f20e13ed70
Ahmet Zurnaci <cengamat@gmail.com>
2021-03-26 11:16:23 +0000

    Style changes

--- react-zone-2/src/App.js
+++ react-zone-2/src/App.js
@@ -1,21 +1,7 @@
 import { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import styled from 'styled-components';
 
-const StyledButton = styled.button`
-background-color: ${ props => props.alt ? 'red' : 'green'};
-color: white;
-font: inherit;
-border: 1px solid blue;
-padding: 8px;
-cursor: pointer;
-
-&:hover {
-  background-color: ${ props => props.alt ? 'lightpink' : 'lighsalmon'};
-  color: black;
-}
-`;
 class App extends Component{
   state = {
     persons : [
@@ -84,7 +70,7 @@ class App extends Component{
       <div className="App">
           <h1>Hello!</h1>
           <p className = {classes.join(' ')}>Who this?</p>
-          <StyledButton alt = {this.state.showPersons} onClick = {this.toggleNameHandler}>Toggle Name</StyledButton>
+          <button className = "button" alt = {this.state.showPersons} onClick = {this.toggleNameHandler}>Toggle Name</button>
           {person}
         </div>
     );
