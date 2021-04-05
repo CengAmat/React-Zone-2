a7d2a7d2ddea195d226d0148a0eccbd081f41a45
Ahmet Zurnaci <cengamat@gmail.com>
2021-04-05 20:55:33 +0100

    Another form of HOCs

--- react-zone-2/src/containers/App.js
+++ react-zone-2/src/containers/App.js
@@ -2,7 +2,8 @@ import { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Aux';
 
 class App extends Component{
   constructor(props) {
@@ -80,7 +81,7 @@ class App extends Component{
     }
 
     return (
-      <WithClass classes = {classes.App}>
+      <Aux>
         <button
           onClick = {() => {
             this.setState({ showCockpit: false });
@@ -97,9 +98,9 @@ class App extends Component{
           />
         ) : null }
           {person}
-        </WithClass>
+        </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
