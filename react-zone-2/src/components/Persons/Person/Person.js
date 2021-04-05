762cf847b0ff67174ad62962b72fbcb23019bcd9
Ahmet Zurnaci <cengamat@gmail.com>
2021-04-05 20:37:14 +0100

    Rendering adjacent JSX elements

--- react-zone-2/src/components/Persons/Person/Person.js
+++ react-zone-2/src/components/Persons/Person/Person.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-        <div className = {classes.Person}>
-            <p onClick = {this.props.click}>{this.props.name} - {this.props.age} years old! </p>
-            <p>{this.props.children}</p>
-            <input type = "text" onChange = {this.props.changed} defaultValue = {this.props.name}/>
-        </div>
+            <Aux>
+                <p onClick = {this.props.click}>{this.props.name} - {this.props.age} years old! </p>
+                <p>{this.props.children}</p>
+                <input type = "text" onChange = {this.props.changed} defaultValue = {this.props.name}/>
+            </Aux>    
         )
     }
 }
-
 
 export default Person;
