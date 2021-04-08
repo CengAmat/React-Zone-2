24d45670e223708cfa11a6dadf487d45e2f06d3f
Ahmet Zurnaci <cengamat@gmail.com>
2021-04-08 14:27:21 +0100

    Switching Between Pages

--- react-http/src/containers/Blog/Blog.js
+++ react-http/src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route } from 'react-router-dom';
 // import axios from "axios";
 import Posts from '../../containers/Blog/Posts/Posts';
+import NewPost from '../Blog/NewPost/NewPost';
 import "./Blog.css";
 
 class Blog extends Component {
@@ -19,6 +20,7 @@ class Blog extends Component {
         {/* <Route path ="/" exact render={() => <h1>Home</h1>}/>
         <Route path ="/new-post" render={() => <h1>Home 2</h1>}/> */}
         <Route path ="/" exact component = {Posts} />
+        <Route path ="/new-post" component = {NewPost} />
       </div>
     );
   }
