455b03870bbbafeb146bb146333413134076d7db
Ahmet Zurnaci <cengamat@gmail.com>
2021-04-09 15:50:48 +0100

    Creating Dynamic Nested Routes

--- react-http/src/containers/Blog/FullPost/FullPost.js
+++ react-http/src/containers/Blog/FullPost/FullPost.js
@@ -9,34 +9,37 @@ class FullPost extends Component {
 
   componentDidMount() {
     console.log(this.props);
+    this.loadData();
+  }
+
+  componentDidUpdate() {
+    this.loadData();
+  }
+
+  loadData() {
     if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
-        (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id)
+        (this.state.loadedPost &&
+          this.state.loadedPost.id !== +this.props.match.params.id)
       ) {
-        axios
-          .get("/posts/" + this.props.match.params.id)
-          .then((response) => {
-            // console.log(response);
-            this.setState({ loadedPost: response.data });
-          });
+        axios.get("/posts/" + this.props.match.params.id).then((response) => {
+          // console.log(response);
+          this.setState({ loadedPost: response.data });
+        });
       }
     }
   }
 
   deleteDataHandler = () => {
-      axios.delete("/posts/" + this.props.id)
-        .then(response => {
-            console.log(response);
-        })
-  }
-  
-
-
+    axios.delete("/posts/" + this.props.match.params.id).then((response) => {
+      console.log(response);
+    });
+  };
 
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
-    if (this.props.id) {
+    if (this.props.match.params.id) {
       post = <p style={{ textAlign: "center" }}>Loading...!</p>;
     }
     if (this.state.loadedPost) {
@@ -45,7 +48,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete" onClick={this.deleteDataHandler}>Delete</button>
+            <button className="Delete" onClick={this.deleteDataHandler}>
+              Delete
+            </button>
           </div>
         </div>
       );
